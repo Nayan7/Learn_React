f4efa653e43bba32c0d40d47639472d3cde3766f
Nayan Joshi <nayanjoshi@Nayans-MBP.lan>
2025-02-01 16:09:55 -0600

    trying how to use react

---
+++ practice_code/app.js
@@ -0,0 +1,3 @@
+const heading = [React.createElement('h1', {}, "Hello World from React"), React.createElement('h1', {}, 'Sibling data')];
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
